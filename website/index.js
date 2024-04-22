454084a9958d79de004205e5b11b81dde4ace774
jsandhu0780 <jashan.sandhu@hotmail.com>
2024-04-22 17:07:21 -0400

    index changes

--- website/index.js
+++ website/index.js
@@ -3,6 +3,6 @@ let counterElement = document.querySelector("#counter-number");
 async function updateCounter() {
     let response = await fetch("https://jxfpdi3idix77b7tgjvrrdrdta0xbxok.lambda-url.us-east-1.on.aws/");
     let data = await response.json();
-    counterElement.innerHTML = `Views: ${data}`;
+    counterElement.innerText = `Views: ${data}`;
 }
-updateCounter();
+await updateCounter();
